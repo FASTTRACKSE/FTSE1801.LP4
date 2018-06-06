c7bd65aee30c7e6371023b5f97c27f615596f8b1
Vũ Văn Mạnh <ftse1801010@st.fasttrack.edu.vn>
2018-06-06 18:12:37 +0700

    bài tập java

--- VanManh/Java/java_unit2/src/java_unit2/bang_diem.java
+++ VanManh/Java/java_unit2/src/java_unit2/bang_diem.java
@@ -4,39 +4,31 @@ import java.util.Scanner;
 
 public class bang_diem {
 	public static void main(String[] args) {
+		String[] hoTen = new String[20];
+		double[] diemHS1 = new double[30];
+		double[] diemHS2 = new double[30];
+		double[] diemtb = new double[20];
 		Scanner input = new Scanner(System.in);
-		String hoc_luc;
-		System.out.print("Nhập tên:");
-		String hoTen =  input.nextLine();
+		System.out.print("Nhập họ tên HS1:");
+		hoTen[0] = input.nextLine();
+		System.out.print("Nhập điểm LP1:");
+		diemHS1[0] = input.nextDouble();
+		System.out.print("Nhập điểm LP2:");
+		diemHS1[1] = input.nextDouble();
+		System.out.print("Nhập điểm LP3:");
+		diemHS1[2] = input.nextDouble();
+		diemtb[0] = (diemHS1[0]+diemHS1[1]+diemHS1[2])/3;
+		System.out.println("Điểm trung bình:"+ diemtb[0]);
 		
-		System.out.print("Nhập điểm lp1:");
-		double lp1 = input.nextDouble();
-		
-		System.out.print("Nhập điểm lp2:");
-		double lp2 = input.nextDouble();
-		
-		System.out.print("Nhập điểm lp3:");
-		double lp3 = input.nextDouble();
-		
-		double dtb = (lp1+lp2+lp3)/3 ;
-		
-		if(dtb<5) {
-			hoc_luc = "Yếu";
-		}else if(dtb<6.5) {
-			hoc_luc = "Trung bình";
-		}else if(dtb<7.5) {
-			hoc_luc = "Khá";
-		}else if(dtb<9) {
-			hoc_luc = "Giỏi";
-		}else {
-			hoc_luc = "Xuất sắc";
-		}
-	
-		System.out.println("Họ và tên:" + hoTen);
-		System.out.println("Điểm lp1 :" + lp1);
-		System.out.println("Điểm lp2 :" + lp2);
-		System.out.println("Điểm lp3 :" + lp3);
-		System.out.println("Điểm tb  :" + dtb);
-		System.out.println("Xếp loại :" + hoc_luc);
+		System.out.print("Nhập họ tên HS2:");
+		hoTen[1] = input.nextLine();
+		System.out.print("Nhập điểm LP1:");
+		diemHS2[0] = input.nextDouble();
+		System.out.print("Nhập điểm LP2:");
+		diemHS2[1] = input.nextDouble();
+		System.out.print("Nhập điểm LP3:");
+		diemHS2[2] = input.nextDouble();
+		diemtb[1] = (diemHS2[0]+diemHS2[1]+diemHS2[2])/3;
+		System.out.print("Điểm trung bình:"+ diemtb[1]);
 	}
 }
