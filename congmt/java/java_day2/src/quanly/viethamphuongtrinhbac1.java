a59408b846faa262e74bb812e13b3c2a93332671
Mai Thành Công <38996823+Congmt@users.noreply.github.com>
2018-06-07 17:15:20 +0700

    nôp bài

---
+++ congmt/java/java_day2/src/quanly/viethamphuongtrinhbac1.java
@@ -0,0 +1,37 @@
+package quanly;
+
+import java.util.Scanner;
+
+
+//import java_day2.quanly;
+
+public class viethamphuongtrinhbac1 {
+	public static void main(String[] args) {
+		quanly.vidu1.giaiPtbac1(args);;
+//		String pt=giaiPtbac1();
+//		System.out.print(pt);
+	}
+//	public static String giaiPtbac1() {
+//		double a,b;
+//		
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Nhập giá trị a: ");
+//		a= sc.nextDouble();
+//		System.out.println("Nhập giá trị b: ");
+//		b= sc.nextDouble();
+//		if(a==0) {
+//			if(b==0) {
+//				System.out.println("- Phương trình có vô số nghiệm.");
+//			}
+//			else {
+//				System.out.println("- Phương trình vô nghiệm.");
+//			}
+//		}else {
+//			double x= -b/a;
+//			System.out.println("- Phương trình có nghiệm là x: " + x);
+//		}
+//		return null;
+//	}
+
+	
+}
