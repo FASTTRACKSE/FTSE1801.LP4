22fd7ea08c1c264bfe68dc20ec7fe91953bcf27c
ngocthien <ftse1801014@st.fasttrack.edu.vn>
2018-06-07 21:39:35 +0700

    up bài tập

---
+++ thienpn/javarcore/java_d3/src/java_d3/Giaipt_bac1.java
@@ -0,0 +1,26 @@
+package java_d3;
+
+import java.util.Scanner;
+
+public class Giaipt_bac1 {
+	public static void pt_bac_1(String[]args){
+		
+		int a,b;
+		Scanner input = new Scanner (System.in);
+		
+		System.out.println("Nhập giá trị a = ");
+		a = input.nextInt();
+		System.out.println("Nhập giá trị b = ");
+		b = input.nextInt();
+		
+		
+		
+		if ((a==0)&&(b==0)){
+			System.out.println("Phương trình vô số nghiệm");
+		} else if ((a==0)&&(b!=0)) {
+			System.out.println("Phương trình vô nghiệm");
+		} else {
+			System.out.println("Phương trình có nghiệm x = " +(-b/a));
+		}
+	}
+}
---
+++ thienpn/javarcore/java_d3/src/java_d3/menu.java
@@ -0,0 +1,34 @@
+package java_d3;
+
+import java.util.Scanner;
+
+public class menu {
+  public static void main(String[]args) {
+	  System.out.println( "1.Giải phương trình bậc 1");
+	  System.out.println( "2.Giải phương trình bậc 2");
+	  System.out.println( "3.Tính tiền điện");
+	  System.out.println("4.kết thúc chương trình");
+	 
+	  Scanner sc = new Scanner(System.in);
+	  int n;
+	  
+	  do {
+		  System.out.println("nhập số thứ tự: ");
+		  n = sc.nextInt();
+	  if(n==1) {
+		  Giaipt_bac1.pt_bac_1(args);
+		  
+	  }else if(n==2) {
+		  Giaipt_bac2.pt_bac_2(args); 
+		  
+	  }else if(n==3) {
+		  tiendien.tien_dien(args);
+		  
+	  }
+	  else if(n==4) {
+		  System.out.println("4.kết thúc chương trình");
+	  }
+	  }
+	  while(n!=4);
+  }
+}
