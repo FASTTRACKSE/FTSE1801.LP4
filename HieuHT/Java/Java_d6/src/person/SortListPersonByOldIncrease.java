6d05b60441b6370e03e7ee3bf51c128b3d255360
hieuhotan <ftse1801004@st.fasttrack.edu.vn>
2018-06-21 11:34:33 +0700

    BaiTap

--- HieuHT/Java/Java_d6/src/person/SortListPersonByOldIncrease.java
+++
@@ -1,9 +0,0 @@
-package person;
-
-import java.util.Comparator;
-
-public class SortListPersonByOldIncrease implements Comparator<Person> {
-	public int compare(Person o1, Person o2) {
-		return o1.getOld() - o2.getOld();
-	}
-}
