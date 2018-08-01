a1761135d86baecfe715a45966a9c9d2a5af015f
ngocthien <ftse1801014@st.fasttrack.edu.vn>
2018-08-01 16:15:28 +0700

    project

---
+++ thienpn/javarcore/Quan Ly Thu Vien/src/quanly_thuvien/model/entity/QuanLy_BanDoc.java
@@ -0,0 +1,66 @@
+package quanly_thuvien.model.entity;
+
+public class QuanLy_BanDoc {
+    private String MaThanhVien;
+    private String tenThanhVien;
+    private String SoNha;
+    private String maThanhPho;
+    private String maQuanHuyen;
+    private String maPhuongXa;
+    private String SDT;
+    private String Email;
+    
+    
+	public String getMaThanhVien() {
+		return MaThanhVien;
+	}
+	public void setMaThanhVien(String maThanhVien) {
+		MaThanhVien = maThanhVien;
+	}
+	public String getTenThanhVien() {
+		return tenThanhVien;
+	}
+	public void setTenThanhVien(String tenThanhVien) {
+		this.tenThanhVien = tenThanhVien;
+	}
+	public String getSoNha() {
+		return SoNha;
+	}
+	public void setSoNha(String soNha) {
+		SoNha = soNha;
+	}
+	public String getMaThanhPho() {
+		return maThanhPho;
+	}
+	public void setMaThanhPho(String maThanhPho) {
+		this.maThanhPho = maThanhPho;
+	}
+	public String getMaQuanHuyen() {
+		return maQuanHuyen;
+	}
+	public void setMaQuanHuyen(String maQuanHuyen) {
+		this.maQuanHuyen = maQuanHuyen;
+	}
+	public String getMaPhuongXa() {
+		return maPhuongXa;
+	}
+	public void setMaPhuongXa(String maPhuongXa) {
+		this.maPhuongXa = maPhuongXa;
+	}
+	public String getSDT() {
+		return SDT;
+	}
+	public void setSDT(String sDT) {
+		SDT = sDT;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	
+	
+	
+    
+}
